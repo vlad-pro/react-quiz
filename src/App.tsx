731686e097400d181757499b282574622d361cd6
vproskurin <vladimir.proskurin@gmail.com>
2020-08-17 23:03:44 -0700

    something is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { fetchQuizQuestions } from './API'
 // Components
 import QuestionCard from './components/QuestionCard'
+// Types
+import { Difficulty } from './API'
+
+const TOTAL_QUESTIONS = 10
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.MEDIUM))
+
   const startQuiz = async () => {}
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
@@ -12,11 +26,22 @@ function App() {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
-      <button className="start" onClick={startQuiz}>Start</button>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
       <p className="score">Score</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
-      <button className="next" onClick={nextQuestion}>Next Question</button>
+      {/* <QuestionCard
+        questionNum={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   )
 }
